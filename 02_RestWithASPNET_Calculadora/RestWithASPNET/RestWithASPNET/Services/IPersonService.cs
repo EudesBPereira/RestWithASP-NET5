45debb438f372587dede13ecd28f5874761d8c7d
Eudes Barbosa Pereira <AB1119384@br.atento.com>
2022-08-13 19:31:15 -0300

    Criacao Mock

---
+++ 02_RestWithASPNET_Calculadora/RestWithASPNET/RestWithASPNET/Services/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNET.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNET.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindByID(long id);
+
+        List<Person> Findall();
+
+        Person Update(Person person);
+
+        void Delete(long id);
+    }
+}
